21d77aca7153ac8a9b30fe7f731d0d41408c1c0f
amrwski <ant.morawski@gmail.com>
2019-04-16 17:31:04 +0200

    completed login/logout handling

---
+++ starter-code/routes/auth.js
@@ -0,0 +1,109 @@
+const express = require("express")
+const bcrypt = require("bcrypt")
+const User = require("../models/user")
+const router = express.Router()
+
+//Render the signup page
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup")
+})
+
+// create login route
+router.get("/login", (req, res, next) => {
+  res.render("auth/login")
+})
+
+//Route to secret page
+router.get("/secret", (req, res, next) => {
+  if (req.session.loggedInUser) {
+    res.render("auth/secret")
+  } else {
+    res.render("error")
+  }
+})
+
+//Route to private page
+router.get("/main", (req, res, next) => {
+  if (req.session.loggedInUser) {
+    res.render("auth/main")
+  } else {
+    res.render("error")
+  }
+})
+
+//Logout
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect("/")
+    console.log("you are logged out")
+  })
+})
+
+//create POST method for logging in
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    res.render("auth/login", {
+      errorMessage: "You need to enter a username and a password"
+    })
+    return
+  }
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "This username was not found"
+        })
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.loggedInUser = true
+        res.redirect("/secret")
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Wrong password mate!"
+        })
+      }
+    })
+    .catch(err => {
+      console.error("Error finding user", err)
+      next()
+    })
+})
+
+//Sign up and create a user
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body
+  const salt = bcrypt.genSaltSync()
+  const hashPassword = bcrypt.hashSync(password, salt)
+
+  //check if password and username are not empty
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "You need to enter a username and a password"
+    })
+    return
+  }
+  //use user schema to create a user and ensure user is new
+  User.findOne({ username }).then(user => {
+    if (user) {
+      res.render("auth/signup", {
+        errorMessage: "There is already a user with this username"
+      })
+    }
+    return
+  })
+  User.create({ username, password: hashPassword })
+    .then(() => {
+      res.redirect("/secret")
+    })
+    .catch(err => {
+      console.error(err)
+      next()
+    })
+    .catch(err => {
+      console.error(`Can't find user`, err)
+      next()
+    })
+})
+
+module.exports = router
